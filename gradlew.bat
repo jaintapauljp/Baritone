6df141f155e708eae61018457dc5335d84e81eec
jaintapauljp <106739373+jaintapauljp@users.noreply.github.com>
2023-06-13 10:32:11 +0600

    Update gradlew.bat for BUET

--- gradlew.bat
+++ gradlew.bat
@@ -17,11 +17,6 @@ set APP_HOME=%DIRNAME%
 set DEFAULT_JVM_OPTS=
 
 @rem Find java.exe
-if defined JAVA_HOME goto findJavaFromJavaHome
-
-set JAVA_EXE=java.exe
-%JAVA_EXE% -version >NUL 2>&1
-if "%ERRORLEVEL%" == "0" goto init
 
 echo.
 echo ERROR: JAVA_HOME is not set and no 'java' command could be found in your PATH.
